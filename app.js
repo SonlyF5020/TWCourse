88bd268dbc71939071e8f610868452ab00302060
Zheng Jianing <jianing.zheng@rea-group.com>
2013-12-22 15:22:24 +0800

    create a course, list all courses both in html and json.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var Course = require('./models/course.js');
 var http = require('http');
 var path = require('path');
-
+var settings = require('./settings');
 var app = express();
 
 // all environments
@@ -20,6 +17,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session({ cookie: { maxAge: 60000 }}));
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,9 +28,50 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+// basic routes
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/api', function (req, res) {
+  res.render('api');
+});
+
+app.get('/success', function (req, res) {
+  res.render('success');
+});
+
+app.get('/error', function (req, res) {
+  res.render('error');
+});
+
+// courses routes
+app.get('/courses', function (req, res) {
+  new Course().all(function(error, docs) {
+    res.render('courses/index', {courses: docs});
+  });
+});
+
+app.get('/courses.json', function (req, res) {
+  new Course().all(function(error, docs) {
+    res.json(docs);
+  });
+});
+
+app.get('/courses/new', function (req, res) {
+  res.render('courses/new');
+});
+
+app.post('/courses/new', function (req, res) {
+  var course = new Course();
+  course.save({name: req.body.name, author: req.body.author, discription: req.body.discription}, function (err) {
+    if (err !== null) {
+      return res.render('error', {errorMessage: err});
+    }
+    res.render('success');
+  });
 });
---
+++ models/course.js
@@ -0,0 +1,88 @@
+var mongodb = require('./db');
+
+function Course(){}
+
+module.exports = Course;
+
+Course.prototype.all = function(callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+
+    db.collection('courses', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+
+      collection.find().sort({time: -1}).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+Course.prototype.save = function(newCourse, callback) {
+
+  if (newCourse.name === '' || newCourse.author === '' || newCourse.discription === '') {
+    return callback('Error: Has empty fields!');
+  }
+
+  var course = {
+    name: newCourse.name,
+    author: newCourse.author,
+    discription: newCourse.discription,
+    created_at: new Date()
+  };
+
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+
+    db.collection('courses', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+
+      collection.insert(course, {safe: true}, function (err, course) {
+        mongodb.close();
+        return callback(null);
+      });
+    });
+  });
+};
+
+Course.get = function (name, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+
+    db.collection('course', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+
+      var query = {};
+      if (name) {
+        query.name = name;
+      }
+
+      collection.find(query).sort({time: -1}).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
